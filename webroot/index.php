247e47206a1d2ef147ddf2725e65e9ef7b295832
alexanderlakhneko <alexanderlakhneko@gmail.com>
2017-02-07 17:17:23 +0200

    Установлен composer и добавлена автозагрузка  основные константы

--- webroot/index.php
+++ webroot/index.php
@@ -1,3 +1,16 @@
 <?php
 
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', __DIR__ . DS . '..' . DS );
+define('SRC_DIR', ROOT . 'src' . DS);
+define('VIEW_DIR', SRC_DIR . 'View' . DS);
+define('LIB_DIR', SRC_DIR . 'Library' . DS);
+define('CONFIG_DIR', ROOT . 'config' . DS);
+define('VENDOR_DIR', ROOT . 'vendor' . DS);
+
+require (VENDOR_DIR . 'autoload.php');
+
 echo 'test';
